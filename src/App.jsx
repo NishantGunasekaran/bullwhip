75379962f99fdd83e5b6fc0727a704115930c0fb
Nishant Gunasekaran <nishantgunasekaran@gmail.com>
2026-05-01 16:23:18 +0530

    message

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Rounds played: {count}
+          Number of Rounds played: {count}
         </button>
       </section>
 
